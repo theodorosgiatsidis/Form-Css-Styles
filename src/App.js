2744ba6f3d18cf10d1b658e7a0b5cd578a20bf92
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2021-11-26 13:16:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
+import React, { useState, useRef } from "react";
+
 import "./App.css";
-import Square from "./components/Square";
-import Square2 from "./components/Square2";
-import Square3 from "./components/Square3";
-import Square4 from "./components/Square4";
-import Square5 from "./components/Square5";
-import Square6 from "./components/Square6";
 
 function App() {
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+  const [background, setBackground] = useState("");
+  const input1 = useRef(null);
+  const input2 = useRef(null);
+  const input3 = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setWidth(input1.current.value);
+    setHeight(input2.current.value);
+    setBackground(input3.current.value);
+  };
   return (
     <div className="App">
-      <Square />
-      <Square2 />
-      <Square3 />
-      <Square4 />
-      <Square5 />
-      <Square6 />
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label> CSS Test </label>
+          <input
+            style={{ width: width }}
+            placeholder="Width"
+            type="text"
+            ref={input1}
+          />
+          <input
+            style={{ height: height }}
+            placeholder="Heigh"
+            type="text"
+            ref={input2}
+          />
+          <input placeholder="Background-color" type="text" ref={input3} />
+        </div>
+
+        <button
+          style={{ backgroundColor: background }}
+          type="submit"
+          id="submitBtn"
+          className="submitBtn"
+        >
+          {" "}
+          submit
+        </button>
+      </form>
     </div>
   );
 }
